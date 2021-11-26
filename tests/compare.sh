09e76eb7fca05af5616b1145f1902e6f84ae58fd
alexjfortuna <alexjfortuna@gmail.com>
2021-11-26 13:20:46 -0500

    Testing framework

--- tests/compare.sh
+++ tests/compare.sh
@@ -1,5 +1,2 @@
-# @param $1: known tutorial output
-# @param $2: workflow output
-diff -bws <(sort$1) <(sort$2)
-# -bw ignores spacing differences
-# -s reports if the two are same
+#!/bin/bash
+diff -s <(sort $1) <(sort $2)
